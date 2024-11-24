12f0296d4def0fd06af1a448c1a977774878ba68
lbatres <lrbg_02@hotmail.com>
2024-11-24 14:22:00 -0600

    Fix Appium startup issue in GitHub Actions workflow

--- wdio.conf.js
+++ wdio.conf.js
@@ -56,7 +56,7 @@ exports.config = {
         platformName: 'Android',
         'appium:deviceName': 'emulator-5554',
         'appium:automationName': 'UiAutomator2',
-        'appium:app': '/Users/luisrogelio/Documents/testAppReservamosJS/apk/gfa22112024.apk'
+        'appium:app': 'https://appsreservamos2024r1.s3.us-east-2.amazonaws.com/gfa22112024.apk' // /Users/luisrogelio/Documents/testAppReservamosJS/apk/gfa22112024.apk
     }],
 
     //
