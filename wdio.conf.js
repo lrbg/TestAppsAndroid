6c13402c8f5d5f03d543c169627d7ec094cbf639
lbatres <lrbg_02@hotmail.com>
2024-11-25 13:13:12 -0600

    3e9jde

--- wdio.conf.js
+++ wdio.conf.js
@@ -100,11 +100,11 @@ exports.config = {
     // baseUrl: 'http://localhost:8080',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
+    waitforTimeout: 30000,
     //
     // Default timeout in milliseconds for request
     // if browser driver or grid doesn't send response
-    connectionRetryTimeout: 300000,
+    connectionRetryTimeout: 600000,
     //
     // Default request retries count
     connectionRetryCount: 3,
