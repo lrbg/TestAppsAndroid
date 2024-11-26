241222cc4769e751e2d1e1e448f79c35c4fa1cfc
lbatres <lrbg_02@hotmail.com>
2024-11-26 12:58:46 -0600

    Used to Did

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -17,5 +17,41 @@ describe('it should complete a simple purchase @e2e_001', () => {
     it('Should validate splash component', async () => {
         await SplashScreen.validateAndCaptureElements();
     });
+    it('You should perform a one-way search', async () => {
+        const origin = searchValues[global.brand]?.origin_text || searchValues.default.origin_text;
+        const destination = searchValues[global.brand]?.destination_text || searchValues.default.destination_text;
+        await SearchPage.searchTripTomorrow(origin, destination);
+    });
+    it('You should make a selection of a trip by schedule', async () => {
+        await SelectTrip.selectRandomChooseButton();
+    });
+    it('Should check and capture available and occupied seats', async () => {
+        const numberOfSeatsToSelect = 1;
+        await Seats.processSeats(numberOfSeatsToSelect);
+    });
+    it('Should load passenger information', async () => {
+        const numberOfPassengers = 1;
+        const passengersData = passengers.slice(0, numberOfPassengers).map(pax => ({
+            name: pax.name,
+            surname: pax.lastName,
+            category: 'Adulto'
+        }));
+        await Pax.loadPassengerData(passengersData);
+    });
+    it('You should select medical insurance', async () => {
+        const addInsurance = true;
+        await Insurance.selectInsuranceOption(addInsurance);
+    });
+    it('You should complete the purchase process', async () => {
+        const purchaserData = purchaserValues;
+        const paymentType = 'creditDebit';
+        await Purchase.fillPurchaseData(purchaserData);
+        await Purchase.selectPaymentOption(paymentType);
+        if (paymentType === 'creditDebit') {
+            await Purchase.fillCardData(creditCardValues);
+        }
+        await Purchase.completePurchase();
+        await Purchase.validateSuccessfulPurchaseMessage();
+    });
     
 });
